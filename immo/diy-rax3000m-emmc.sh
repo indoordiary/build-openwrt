d88bb1c853572c3ad20b5e12ae44ee848f2c1d1f
indoordiary <lfn2333333@gmail.com>
2024-08-10 12:58:53 +0800

    github action update

--- immo/diy-rax3000m-emmc.sh
+++ immo/diy-rax3000m-emmc.sh
@@ -25,8 +25,8 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git -b v0.9 package
 # sed -i '/+luci-light/d;s/+luci-app-opkg/+luci-light/' ./feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' ./feeds/luci/collections/luci-light/Makefile
 ## 修改argon背景图片
-rm -rf feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f $GITHUB_WORKSPACE/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+rm -rf package/new/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/bg1.jpg package/new/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 ## Add luci-app-wechatpush
 # rm -rf feeds/luci/applications/luci-app-wechatpush
